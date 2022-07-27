fa0bb81606c6f035485020c943d9f47a94da5edd
Im <asd.afe.htc@gmail.com>
2022-07-27 19:02:59 +0200

    first commit

---
+++ JScourse.js
@@ -0,0 +1,59 @@
+// const Surname = 'Donichenko'
+// const Name = 'Andrii'
+// let myAge = 23
+// const tr = true
+// myAge = 23
+
+// // const a = 10
+// // const b = 5
+// // a - b
+// // a + b
+// // a / b
+// // a * b
+// // let c = 5
+// // c += b
+// // let curentYear = 2022
+// // const isx = a - b < c
+// // console.log(isx)
+// function NameSurname(Name, Surname) {
+// 	console.log(Name)
+// }
+
+// let = NameSurname(Andrii, Donichenko)
+// console.log(Name)
+
+// let buttons = [b1, b2]
+// let kategorii = ['A1', 'A']
+
+// for (let btn of buttons) {
+// 	btn.onclick = function OnClick(index) {
+// 		btn.classList.toggle('b')
+// 		btn = kategorii
+// 		for (let kat of kategorii)
+// 			document.getElementById('inp1').value = b1.value
+// 		console.log(i);
+// 	}
+
+// }
+// function Myf(name) {
+// 	console.log(typeof name);
+// }
+
+// Myf(5)
+
+// function playerRankUp(points) {
+// 	if (points >= 100) {
+// 		return console.log('Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.')
+// 	}
+// 	else {
+// 		return console.log('you are failed')
+// 	}
+
+// }
+
+// playerRankUp(99)
+
+function maps(x) {
+	for (var maps = [], i = 0; i < 40; ++i) a[i] = i;
+	let y = maps.map(num => num * 2)
+}
