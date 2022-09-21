c27c09fd3267e3c24505d10d0db4b4326bec914f
Im <asd.afe.htc@gmail.com>
2022-09-21 22:14:48 +0200

    79.1

--- JScourse.js
+++ JScourse.js
@@ -2457,28 +2457,158 @@
 // }
 // console.log(perimeter(4));
 
-78
+// 78
 
-function nextBigger(n){
-	console.log(n);
-	var chars = n.toString().split('');
-	var i = chars.length-1;
-	while(i > 0) {
-	  if (chars[i]>chars[i-1]) break;
-	  i--;
-	}
-	if (i == 0) return -1;
-	var suf2 = chars.splice(i)
-	var suf = suf2.sort();
-	var t = chars[chars.length-1];
-	for (i = 0; i < suf.length; ++i) {
-	  if (suf[i] > t) break;
-	}
-	chars[chars.length-1] = suf[i]
-	suf[i] = t;
-	var res = chars.concat(suf);
-	var num = parseInt(res.join(''));
-	console.log("->" +num);
-	return num;
+// function nextBigger(n){
+// 	console.log(n);
+// 	var chars = n.toString().split('');
+// 	var i = chars.length-1;
+// 	while(i > 0) {
+// 	  if (chars[i]>chars[i-1]) break;
+// 	  i--;
+// 	}
+// 	if (i == 0) return -1;
+// 	var suf2 = chars.splice(i)
+// 	var suf = suf2.sort();
+// 	var t = chars[chars.length-1];
+// 	for (i = 0; i < suf.length; ++i) {
+// 	  if (suf[i] > t) break;
+// 	}
+// 	chars[chars.length-1] = suf[i]
+// 	suf[i] = t;
+// 	var res = chars.concat(suf);
+// 	var num = parseInt(res.join(''));
+// 	console.log("->" +num);
+// 	return num;
+//    }
+//    console.log(nextBigger(981932));
+
+// const sortedDigits = n => { let arr = n.toString().split(''); arr.sort((a, b) => b - a); return arr; };
+
+// function nextBigger(n) {
+
+// 	let arr = sortedDigits(n);
+// 	let max = parseInt(arr.join(''), 10);
+
+// 	for (var i = n + 1; i <= max; i++) {
+// 		let sorti = sortedDigits(i)
+// 		if (sorti.every((x, j) => x === arr[j])) {
+// 			return i;
+// 		}
+// 	}
+
+// 	return -1;
+// }
+// console.log(nextBigger(981932));
+
+// function nextBigger(n) {
+// 	if (n === reverseSortDigits(n)) return -1
+
+// 	let nSorted = sortDigits(n)
+
+// 	while (true) {
+// 	  n += 1
+// 	  if (sortDigits(n) === nSorted) return n
+// 	}
+//    }
+
+//    function splitDigits(d) {
+// 	return String(d).split('')
+//    }
+
+//    function sortDigits(d) {
+// 	return Number(splitDigits(d).sort().join(''))
+//    }
+
+//    function reverseSortDigits(d) {
+// 	return Number(splitDigits(d).sort().reverse().join(''))
+//    }
+//    console.log(nextBigger(12));
+
+// function getIndex(arr) {
+// 	let leftMostIndexOfNumsToChange = 0;
+
+// 	// finds index starting from right most digit then comparing to digit to the left.
+// 	// once we find a left digit that is smaller than the right digit, break loop and return the index of left digit
+// 	for (let i = arr.length - 1; i >= 0; i--) {
+// 	  if (arr[i] > arr[i - 1]) {
+// 	    leftMostIndexOfNumsToChange = i - 1;
+// 	    break;
+// 	  };
+// 	}
+
+// 	return leftMostIndexOfNumsToChange;
+//    }
+
+//    function getClosestGreaterNum(numToCompareTo, arr) {
+// 	let greaterNum = 10;
+// 	let indexOfClosestGreaterNum = -1;
+
+// 	arr.forEach((digit, i) => {
+// 	  if (digit > numToCompareTo && digit < greaterNum) {
+// 	    greaterNum = digit;
+// 	    indexOfClosestGreaterNum = i;
+// 	  };
+// 	});
+
+// 	return indexOfClosestGreaterNum;
+//    }
+
+//    function nextBigger(n){
+// 	// make number an array of single digits
+// 	const arr = n.toString().split('');
+// 	// get index of left most digit that will need to be shuffled around
+// 	const leftMostIndex = getIndex(arr);
+// 	// store left most number for later comparison
+// 	const numToCompareTo = arr[leftMostIndex];
+// 	// create new array by taking out digits that will shift from old array (we'll combine these later) and sort the digits from smallest to biggest
+// 	let availableDigitsToShift = arr.splice(leftMostIndex, arr.length).sort();
+// 	// get the index of the array digit that is the closest greater number compared to the smallest number
+// 	const indexOfClosestGreaterNum = getClosestGreaterNum(numToCompareTo, availableDigitsToShift);
+// 	// move closest greater num digit to front and shift the rest right
+// 	availableDigitsToShift.splice(0, 0, availableDigitsToShift.splice(indexOfClosestGreaterNum, 1)[0]);
+// 	// combine both arrays, turn to string and then to number
+// 	const final = parseInt(arr.concat(availableDigitsToShift).join(''));
+
+// 	return (final > n ? final : -1);
+//    }
+//    console.log(nextBigger(12));
+// overview
+// 10755
+// 15057
+// find out what numbers are going to need to be shuffled by looking at last number (5) and compare to next digit to the left. 5 is <= to 5 so move left again. 5 is <= 7 so move left again. 7 is not >= 0 so we know 0755 will all need to shift around.
+// the numbers shifting/sorting is first based on the 0. we find the next largest available number that is greater than 0. that's 5. next we place the remaining numbers from smallest to largest after the 5 (057).
+
+// function nextBigger(n){
+// 	var arr = n.toString().split("").reverse();
+// 	var i = arr.findIndex((d, _i) => d < arr[_i-1]);
+// 	if (i === -1) { return -1; }
+// 	var subarr = arr.slice(0, i);
+// 	var j = subarr.findIndex((d) => d > arr[i]);
+// 	subarr.splice(j, 1, arr[i]);
+// 	let arr2 = arr.slice(i+1)
+// 	return parseInt(arr2.reverse().concat(arr[j]).concat(subarr).join(""));
+//    }
+//    console.log(nextBigger(12));
+
+// function nextBigger(n) {
+// 	var left = ("" + n).split('');
+// 	var right = [left.pop()];
+// 	while (left.length) {
+// 		right.unshift(left.pop())
+// 		if (right[0] < right[1]) {
+// 			var curr = right[0];
+// 			right.sort();
+// 			var smaller = right.filter(n => n <= curr)
+// 			var larger = right.filter(n => n > curr)
+// 			return +(left.join('') + larger.shift() + smaller.join('') + larger.join(''));
+// 		}
+// 	}
+// 	return -1;
+// }
+// console.log(nextBigger(12));
+
+79
+snail = function(array) {
+	
    }
-   console.log(nextBigger(981932));
