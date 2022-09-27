b3d7a919a371c707d3107fafcd297ca8eeb825de
Im <asd.afe.htc@gmail.com>
2022-09-27 21:58:13 +0200

    81.2

--- JScourse.js
+++ JScourse.js
@@ -2901,7 +2901,27 @@
 // console.log(noquit.match(qRegex))
 
 
-// 81
-// function validSolution(board){
-	
-//    }
+81
+function validSolution(board) {
+	let sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+	let res = []
+	let res2 = []
+	let result = 0
+	for (let i = 0; i < board.length; i++) {
+		for (let ind = 0; ind < board.length; ind++) {
+			result += board[ind][i]
+		}
+		res.push(result)
+		result = 0
+	}
+	return board.every(e=>e.reduce((p,c)=> p+c)==sum) && res.every(el=>el==sum)? true:false
+}
+console.log(validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
+[6, 7, 2, 1, 9, 5, 3, 4, 8],
+[1, 9, 8, 3, 4, 2, 5, 6, 7],
+[8, 5, 9, 7, 6, 1, 4, 2, 3],
+[4, 2, 6, 8, 5, 3, 7, 9, 1],
+[7, 1, 3, 9, 2, 4, 8, 5, 6],
+[9, 6, 1, 5, 3, 7, 0, 8, 4],
+[2, 8, 7, 4, 1, 9, 6, 3, 5],
+[3, 4, 5, 2, 8, 6, 1, 7, 9]]));
