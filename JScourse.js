e9a9fa1f87f952c9b4bcd374c16cf856560c6348
Im <asd.afe.htc@gmail.com>
2022-10-03 22:03:10 +0200

    81.14

--- JScourse.js
+++ JScourse.js
@@ -3041,3 +3041,62 @@
 		// 	[9, 6, 1, 5, 3, 7, 0, 8, 4],
 		// 	[2, 8, 7, 4, 1, 9, 6, 3, 5],
 		// 	[3, 4, 5, 2, 8, 6, 1, 7, 9]]));
+
+		// const validSolution = (board) => {
+		// 	const suok = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		// 	const columns = [[], [], [], [], [], [], [], [], []];
+		// 	const grid = [[], [], [], [], [], [], [], [], []];
+			
+		// 	// Grid and Column creation
+		// 	for (let i = 0; i < board.length; i++) {
+		// 	  board[i].forEach((elem, index) => {
+		// 	    columns[index].push(elem);
+		// 	    if (i < 3) {
+		// 		 if (index < 3) {
+		// 		    grid[0].push(elem);
+		// 		 } else if (index < 6) {
+		// 		    grid[1].push(elem);
+		// 		 } else if (index < 9) {
+		// 		    grid[2].push(elem);
+		// 		 }
+		// 	    }
+		// 	    if (i >= 3 && i <= 5) {
+		// 		 if (index < 3) {
+		// 		    grid[3].push(elem);
+		// 		 } else if (index < 6) {
+		// 		    grid[4].push(elem);
+		// 		 } else if (index < 9) {
+		// 		    grid[5].push(elem);
+		// 		 }
+		// 	    }
+		// 	    if ( i > 5) {
+		// 		 if (index < 3) {
+		// 		    grid[6].push(elem);
+		// 		 } else if (index < 6) {
+		// 		    grid[7].push(elem);
+		// 		 } else if (index < 9) {
+		// 		    grid[8].push(elem);
+		// 		 }
+		// 	    }
+		// 	  })
+		// 	}
+			
+		// 	// Row, Columns and Grid check
+		// 	for (let i = 0; i < columns.length; i++) {
+		// 	   const isSudokuColumns = suok.every(elem => columns[i].indexOf(elem) > -1);
+		// 	   const isSudokuGrid = suok.every(elem => grid[i].indexOf(elem) > -1);
+		// 	   const isSudokuRows = suok.every(elem => board[i].indexOf(elem) > -1);
+		// 	   if (!isSudokuColumns || !isSudokuGrid || !isSudokuRows) return false;
+		// 	}
+			
+		// 	return true
+		//    }
+		//    console.log(validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
+			// 	[6, 7, 2, 1, 9, 5, 3, 4, 8],
+			// 	[1, 9, 8, 3, 4, 2, 5, 6, 7],
+			// 	[8, 5, 9, 7, 6, 1, 4, 2, 3],
+			// 	[4, 2, 6, 8, 5, 3, 7, 9, 1],
+			// 	[7, 1, 3, 9, 2, 4, 8, 5, 6],
+			// 	[9, 6, 1, 5, 3, 7, 0, 8, 4],
+			// 	[2, 8, 7, 4, 1, 9, 6, 3, 5],
+			// 	[3, 4, 5, 2, 8, 6, 1, 7, 9]]));
